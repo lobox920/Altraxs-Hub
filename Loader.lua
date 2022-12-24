f34a09b89f2c81ec20ea5e8430da4d010b43ec54
Awaken <93601997+Awakenchan@users.noreply.github.com>
2022-12-24 12:47:39 +0500

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,3 +1,4 @@
 if game.PlaceId == 2534724415 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/lobox920/Altraxs-Hub/main/Emergency%20Response%3A%20Liberty%20County.lua"))()
+  rconsoleprint("\n in case of ban dm owner or ping Awaken")
 end
