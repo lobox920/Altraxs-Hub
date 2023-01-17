d01f4b01dc5c804d506a140d40acfb8b62f292e6
Awaken <93601997+Awakenchan@users.noreply.github.com>
2023-01-18 02:20:29 +0500

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,3 +1,6 @@
 if game.PlaceId == 2534724415 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/lobox920/Altraxs-Hub/main/ER%3ALC", true))()
  end
+if  game:GetService("CoreGui"):FindFirstChild("DevConsoleMaster") then 
+	game:GetService("CoreGui").DevConsoleMaster:Destroy()
+end
