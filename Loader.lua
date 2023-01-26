9a8d7adca6dfe1613dfcd460b466f468899e54de
Awaken <93601997+Awakenchan@users.noreply.github.com>
2023-01-26 15:40:38 +0500

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,6 +1,12 @@
 if game.PlaceId == 2534724415 then
+  if  game.PlaceVersion ~= 2384 then
+        game.Players.LocalPlayer:Kick("Game updated tell Awaken report to https://discord.gg/3dPPvsVAy4 or Awaken#6636  ")
+        rconsoleinfo("\n Game updated please report to Awaken#6636 or in discord")
+elseif game.PlaceVersion == 2384 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/IceMinisterq/Altraxs-Hub/main/ER%3ALC", true))()
-   end
-  if  game:GetService("CoreGui"):FindFirstChild("DevConsoleMaster") then 
+end
+end
+
+if game:GetService("CoreGui"):FindFirstChild("DevConsoleMaster") then 
       game:GetService("CoreGui").DevConsoleMaster:Destroy()
-  end
+end
