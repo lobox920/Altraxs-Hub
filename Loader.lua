0fcb4fece6cd146a8ec848e71d8740fff4170c6b
Awaken <93601997+Awakenchan@users.noreply.github.com>
2023-02-05 04:31:25 +0500

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,8 +1,16 @@
+local N=game:GetService("VirtualInputManager")
+N:SendKeyEvent(true,"F9",false,game)
+local N=game:GetService("VirtualInputManager")
+N:SendKeyEvent(true,"F9",false,game)
+local N=game:GetService("VirtualInputManager")
+N:SendKeyEvent(true,"F9",false,game)
+local N=game:GetService("VirtualInputManager")
+N:SendKeyEvent(true,"F9",false,game)
 if game.PlaceId == 2534724415 then
-  if  game.PlaceVersion ~= 2395 then
+  if  game.PlaceVersion ~= 2400 then
         game.Players.LocalPlayer:Kick("Game updated tell Awaken report to https://discord.gg/3dPPvsVAy4 or Awaken#6636  ")
         rconsoleinfo("\n Game updated please report to Awaken#6636 or in discord")
-elseif game.PlaceVersion == 2395 then
+elseif game.PlaceVersion == 2400 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/IceMinisterq/Altraxs-Hub/main/ER%3ALC", true))()
 end
 end
